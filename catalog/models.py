16fcbe942bec211811da92449de4b6eebad85ece
qqq <aswd25@yandex.ru>
2022-01-25 16:28:31 +0200

    ADD IMAGEFIELD

--- catalog/models.py
+++ catalog/models.py
@@ -9,6 +9,7 @@ class Fighter(models.Model):
     first_name = models.CharField(max_length=100, help_text='First name of the fighter')
     last_name = models.CharField(max_length=100, help_text='Last name of the fighter')
     nickname = models.CharField(max_length=100, help_text='Nickname of the fighter, if exist', null=True, blank=True)
+    fighter_image = models.ImageField(upload_to='images/', null=True, blank=True)
     birthday = models.DateField('Birthday', null=True, blank=True)
     start_carrier = models.DateField('Start carrier', null=True, blank=True)
     end_carrier = models.DateField('End carrier', null=True, blank=True)
--- catalog/urls.py
+++ catalog/urls.py
@@ -5,6 +5,6 @@ from catalog import views
 urlpatterns = [
     path('', views.index, name='index'),
     path('fighters/', views.FighterListView.as_view(), name='fighters'),
-    re_path('^fighter/(?P<pk>\d+)$', views.FighterDetailView.as_view(), name='fighter-detail')
+    re_path('^fighter/(?P<pk>\d+)$', views.FighterDetailView.as_view(), name='fighter-detail'),
 
 ]
